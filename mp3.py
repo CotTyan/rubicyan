3c19456b543fd0a29d9141bae2d3560a5ef3f834
Laminne Tatsuto Yamamoto <gtattoda4@gmail.com>
2020-08-03 09:26:10 +0900

    Update mp3.py

--- mp3.py
+++ mp3.py
@@ -16,10 +16,21 @@ async def on_message(message):
     if message.content == ("///rubi start"):
         voich = await discord.VoiceChannel.connect(message.author.voice.channel)
         t = random.choice(('music.mp3','music2.mp3','music3.mp3'))
-        #print(t)
+        print(t)
         voich.play(discord.FFmpegPCMAudio(t))
         sleep_time = 62
         await asyncio.sleep(sleep_time)
         await voich.disconnect()
-
+    if t == ("music.mp3"):
+        if message.content == ('春すぎて 夏来にけらし 白妙の 衣ほすてふ 天の香具山'):
+            await message.channel.send("正解")
+        await message.channel.send(t)
+    if t == ('music2.mp3'):
+        # if message.content == ('田子の浦に うち出でてみれば 真白にそ 富士の高嶺に 雪は降りつつ'):
+        #     await message.channel.send("正解")
+        await message.channel.send(t)
+    if t == ('music3.mp3'):
+        if message.content == ('憶良らは 今は罷らむ 子泣くらむ それその母も 我を待つらむそ'):
+            await message.channel.send("正解")
+        await message.channel.send(t)
 client.run("")
