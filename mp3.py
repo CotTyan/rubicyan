a4cad62370247b02b73c470f429d37d985acbead
Laminne Tatsuto Yamamoto <gtattoda4@gmail.com>
2020-08-03 12:38:48 +0900

    Update mp3.py

--- mp3.py
+++ mp3.py
@@ -13,6 +13,12 @@ async def on_message(message):
 
     global voich
     # voice = await join_voice_channel(client.get_channel())
+    if message.content == ("///rubi help"):
+        help = random.choice(('ほととぎす　鳴きつる方を　ながむれば　ただ有明の　月ぞ残れる','ひさかたの　光のどけき　春の日に　静心なく　花の散るらむ','天の原　ふりさけ見れば　春日なる　三笠の山に　出でし月かも'))
+        embed = discord.Embed(title="ヘルプ", description="このヘルプを表示 `///rubi help`\nVCに接続する `///rubi start`\n※この機能はVCに接続しないと使えません　なお、音が大きい場合があるので注意してください\n機能追加はまだまだこれからですので気を長くしてお待ちください", color=0x00ccff)
+        embed = embed.set_author(name="RubiTyan V by laminne", url="https://github.com/laminne", icon_url="")
+        embed = embed.set_footer(text=help)
+        await message.channel.send(embed=embed)
     if message.content == ("///rubi start"):
         voich = await discord.VoiceChannel.connect(message.author.voice.channel)
         t = random.choice(('music.mp3','music2.mp3','music3.mp3'))
